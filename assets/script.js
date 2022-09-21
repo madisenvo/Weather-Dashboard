0382f9b55a80229e475b108cd531654a9e056e90
Madisen Vogel <madisen@Admins-Mac-mini.lan>
2022-09-21 15:59:08 -0600

    building javascript

--- assets/script.js
+++ assets/script.js
@@ -1,55 +1,47 @@
-var citySelection = document.querySelector("#city").value;
-var submitBtn = document.querySelector("#submitBtn");
-var userInputEl = $('#user-input')
-var inputEl = $('.form-input')
+const cityInput = $(".city-input");
+const submitBtn = $("#submitBtn");
+const userFormEl = $('#user-form');
+const searchHistoryEl = $("#search-history");
+const fiveDayForecastEl = $("#five-day-forecast");
+
 
 var APIKey = "019a7bf4afa5e4a22e320f4131dc54fc";
-var city = userFormEl.input.value;
 
 
+// none of this is working
+var formSubmitHandler = function (event){
+  event.preventDefault();
+  var city = cityInput.value.trim();
 
-userFormEl.addEventListener('submit', function (e) {
-    e.preventDefault();
-    var city = citySelection.value;
+  console.log(city);
+}
 
-    
-  
-    // var username = nameInputEl.value.trim();
-    
-  
-    // if (username) {
-    //   getUserRepos(username);
-  
-    //   repoContainerEl.textContent = '';
-    //   nameInputEl.value = '';
-    // } else {
-    //   alert('Please enter a GitHub username');
-    // }
-    console.log("hello from" + city);
-  });
+userFormEl.addEventListener('submit', formSubmitHandler)
 
-
-console.log("hello from" + city);
-
-// // getting data from API
-//   var getCityWeather = function () {
-//     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;  
-//     fetch(queryURL)
-//       .then(function (response) {
-//         if (response.ok) {
-//           console.log(response);
-//           response.json().then(function (data) {
-//             console.log(data);
-//           });
-//         } else {
-//           alert('Error: ' + response.statusText);
-//         }
-//       })
-//       .catch(function (error) {
-//         alert('Unable to access weather data.');
-//       });
-//   };
+// getting data from API
+  var getCityWeather = function () {
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;  
+    fetch(queryURL)
+      .then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert('Unable to access weather data.');
+      });
+  };
 
 // getting 5 day forecast
-var forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + city + "&appid=" + APIKey;
+let cityID = response.data.id;
+var forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&appid=" + APIKey;
 fetch(forecastQueryURL)
+  .then(function(response){
+    fiveDayForecastEl.classList.remove("d-none");
+    // for loop to append details to divs - something like...
+    // const forecastEls = document.querySelectorAll(".forecast");
+    // for (i = 0; i < forecastEls.length; i++) {
+  });
