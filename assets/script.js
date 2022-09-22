34cd96eb4e59f43e6c9a893f4edc35614eabc595
Madisen <madisenvogel@gmail.com>
2022-09-22 17:44:18 -0600

    debugged input

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var cityInput = $(".city-input");
+var cityInput = $("#city");
 var submitBtn = $("#submitBtn");
 var userFormEl = $('#user-form');
 var searchHistoryEl = $("#search-history");
@@ -6,10 +6,12 @@ var fiveDayForecastEl = $("#five-day-forecast");
 var previousSearch = '';
 var currentCity = '';
 
+//syntax for date forecadst
+//res.dt_txt.split(" ")[0];
 
 var APIKey = "019a7bf4afa5e4a22e320f4131dc54fc";
 
-city = "Denver"
+
 
 // fetch error handler
 var handleErrors = function(response){
@@ -19,27 +21,30 @@ var handleErrors = function(response){
 }};
 
 // getting data from API
-function getCityWeather() {
+function getCityWeather(newCity) {
+  //e.preventDefault();
+  //var newCity = cityInput.value;
+  //console.log(newCity);
 
-
-  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;  
-  fetch(queryURL)
-  .then(handleErrors)
+  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + newCity + "&appid=" + APIKey + "&units=imperial";  
+  fetch(queryURL) 
   .then(function (response) {
     return response.json();
   })
-  .then(function (response) {
-    console.log(response);
-    saveSearch(city);
+  .then(function (res) {
+    console.log("Res: " +JSON.stringify(res));
+    saveSearch(newCity);
     renderCity();
     // getForecast();
 
+
+
     let cityWeatherHTML = `
-      <h3>${response.name}></h3>
-      <ul class="list-unstyled">
-        <li>Temperature: ${response.main.temp}&#8457;</li>
-        <li>Humidity: ${response.main.humidity}%</li>
-        <li>Wind Speed: ${response.wind.speed} mph</li>
+      <h3>${res.name} ${res.dt} <img src= https://openweathermap.org/img/w/${res.weather[0].icon}.png> </h3>
+      <ul class="list-unstyled p-3 m-3 bg-primary text-light" style="width: 12rem">
+        <li>Temperature: ${res.main.temp}°F</li>
+        <li>Humidity: ${res.main.humidity}%</li>
+        <li>Wind Speed: ${res.wind.speed} mph</li>
       </ul>`;
   $('#city-weather').html(cityWeatherHTML);
   });
@@ -81,11 +86,11 @@ function renderCity(){
             currentCity=previousSearch;
         }
         if (city === currentCity) {
-            cityEl = `<button type="button" class="list-group-item list-group-item-action active">${city}</button></li>`;
+            cityEl = `<button type="button" class="list-group-item list-group-item-action">${city}</button></li>`;
         } else {
             cityEl = `<button type="button" class="list-group-item list-group-item-action">${city}</button></li>`;
         } 
-        $('#search-history').prepend(cityEl);
+        $('#search-history').append(cityEl);
         }
 }}
 
@@ -106,12 +111,15 @@ function renderCity(){
   // seach button event listener
 submitBtn.on("click", function(event){
   event.preventDefault();
-  getCityWeather(event);
+ var temp = $("#city").val();
+ console.log(temp);
+  getCityWeather(temp);
 });
 
 $("#search-history").on("click", function(event){
   event.preventDefault();
-  $("#city-search").val(event.target.textContent);
-  currentCity=$("#city-search").val();
-  getCityWeather(event);
-})
+  var t = event.target.textContent;
+  //$("#city-search").val(event.target.textContent);
+  //currentCity=$("#city-search").val();
+  getCityWeather(t);
+});
