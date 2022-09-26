7fa2b8f52df954316e6358de3c253ae1437619dd
Madisen Vogel <madisen@Admins-Mac-mini.lan>
2022-09-26 10:27:22 -0600

    added comments

--- assets/script.js
+++ assets/script.js
@@ -16,8 +16,10 @@ function getCityWeather(newCity) {
   })
   .then(function (res) {
     console.log("Res: " +JSON.stringify(res));
+// calling functions to save city to local storage & render history to page
     saveSearch(newCity);
     showHistory();
+// prints weather data to page
     let cityWeatherHTML = `
       <div class="card col-11 m-5">
         <h2>${res.name} ${moment().format("(MM/DD/YY)")} <img src= https://openweathermap.org/img/w/${res.weather[0].icon}.png> </h2>
@@ -31,8 +33,6 @@ function getCityWeather(newCity) {
 
 })};
 
-
-// save to local storage
 var saveSearch = function(newSearch){
   let repeat = false;
 // Check if search in local storage
@@ -48,7 +48,7 @@ var saveSearch = function(newSearch){
   }
 }
 
-// create function to render search history to page
+// renders search history to page as a button
 function showHistory(){
   $('#search-history').empty();
   if (localStorage.length===0){
@@ -72,6 +72,7 @@ function showHistory(){
         }
 }}
 
+// gets 5 day forecast
 function getForecast(newCity){
   let forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + newCity + "&units=imperial" + "&appid=" + APIKey;
   fetch(forecastQueryURL)
@@ -83,7 +84,7 @@ function getForecast(newCity){
       let forecastHTML = `
         <h3>5-Day Forecast:</h3>
         <div ml-10></div>`;
-        // Loop over the 5 day forecast and build the template HTML using UTC offset and Open Weather Map icon
+        // Loop over the 5 day forecast and prints to page - uses utc offset to get date
         for (let i = 0; i < res.list.length; i++) {
             let day = res.list[i];
             let dayTime = day.dt;
@@ -91,6 +92,7 @@ function getForecast(newCity){
             let timeZoneHours = timeZone / 60 / 60;
             let thisMoment = moment.unix(dayTime).utc().utcOffset(timeZoneHours);
             let iconURL = "https://openweathermap.org/img/w/" + day.weather[0].icon + ".png";
+            // only displays weather conditions for middle of day
             if (thisMoment.format("HH:mm:ss") === "11:00:00" || thisMoment.format("HH:mm:ss") === "12:00:00" || thisMoment.format("HH:mm:ss") === "13:00:00") {
                 forecastHTML += `
                 <div class="forecast">
@@ -102,10 +104,7 @@ function getForecast(newCity){
                       <li>Wind Speed: ${day.wind.speed} mph</li>
                   </ul>
                 </div>`;
-            // Build the HTML template
         forecastHTML += `</div>`;
-        
-            // Append the five-day forecast to the DOM
         $('#five-day-forecast').html(forecastHTML);
 }}})}
 
@@ -117,7 +116,8 @@ submitBtn.on("click", function(event){
   getForecast(temp);
 });
 
-$("#search-history").on("click", function(event){
+// makes search history buttons clickable
+searchHistoryEl.on("click", function(event){
   event.preventDefault();
   var historyBtn = event.target.textContent;
   getCityWeather(historyBtn);
